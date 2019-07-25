e51f713f73a20d4ec7dfdb6463aef1675e5198e5
Frank L. Morales II <altf2o@gmail.com>
2019-07-25 09:18:03 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a repository I'm using to track my own progress through Peter Shirley's [Ray Tracing In One Weekend](http://www.realtimerendering.com/raytracing/Ray%20Tracing%20in%20a%20Weekend.pdf)
+
+I will update this README with more helpful information ASAP. 
+
+
+
+
---
+++ src/vec3.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <math.h>
+
+class vec3
+{
+public:
+    // Constructors
+    vec3() {}
+    vec3(float e0, float e1, float e2) { 
+        e[0] = e0; 
+        e[1] = e1;
+        e[2] = e2;
+    }
+
+    // Simple member functions
+    inline float x() const { return e[0]; }
+    inline float y() const { return e[1]; }
+    inline float z() const { return e[2]; }
+    inline float r() const { return e[0]; }
+    inline float g() const { return e[1]; }
+    inline float b() const { return e[2]; }
+    
+    inline const vec3& operator+() const { return *this; }
+    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    inline float operator[](int i) const { return e[i]; }
+    inline float& operator[](int i) { return e[i]; }
+
+    inline float length() const {
+        return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    }
+
+    inline float squared_length() const {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+    // Member function prototypes
+    inline void make_unit_vector();
+    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator-=(const vec3 &v2);
+    inline vec3& operator*=(const vec3 &v2);
+    inline vec3& operator/=(const vec3 &v2);
+    inline vec3& operator*=(const float t);
+    inline vec3& operator/=(const float t);
+
+    // Member variables
+    float e[3];
+};
+
+// Standalone function prototypes
+std::istream& operator>>(std::istream &is, vec3 &t);
+std::ostream& operator<<(std::ostream &os, const vec3 &t);
+float dot(const vec3 &v1, const vec3 &v2);
+vec3 cross(const vec3 &v1, const vec3 &v2);
+vec3 operator+(const vec3 &v1, const vec3 &v2);
+vec3 operator-(const vec3 &v1, const vec3 &v2);
+vec3 operator*(const vec3 &v1, const vec3 &v2);
+vec3 operator/(const vec3 &v1, const vec3 &v2);
+vec3 operator*(float t, const vec3 &v);
+vec3 operator/(vec3 v, float t);
+vec3 operator*(const vec3 &v, float t);
+vec3 unit_vector(vec3 v);
+
